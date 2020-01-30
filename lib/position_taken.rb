bdc0eec926357497caaabbe4e1cabcaf0d479726
MJU <marie.ju2012@outlook.com>
2020-01-30 20:26:12 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,9 @@
 # code your #position_taken? method here!
 def position_taken(board, index)
-  if board[index.to_i - 1] == " "
+  if board[index] == " "
     return false
   else
     return true
   end
 end
+
